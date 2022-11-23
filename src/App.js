01db5e201c0008f20497a99a89df3f54f1b6f541
S. G. Opferman <sophieopferman6@gmail.com>
2022-11-23 01:18:27 -0500

    got it to run without errors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import SplitPane, {
-  Divider,
-  SplitPaneBottom,
-  SplitPaneLeft,
-  SplitPaneRight,
-  SplitPaneTop,
+	Divider,
+	SplitPaneBottom,
+	SplitPaneLeft,
+	SplitPaneRight,
+	SplitPaneTop,
 } from "./components/SplitPane";
 import { useState } from "react";
-import ReferencePaneCtxt from './components/ReferencePaneCtxt';
+import ReferencePaneCtxt from "./components/ReferencePaneCtxt";
 
 function App() {
-  return (
-<div className="App">
-      <ReferencePaneCtxt.Provider value={{}}>
-      {/* quotes, currQuote, setCurrQuote  */}
-        <SplitPane className="split-pane-row">
-          <SplitPaneLeft>
-            <SplitPane className="split-pane-col">
-              <SplitPaneTop />
-              <Divider className="separator-row" />
-              <SplitPaneBottom />
-            </SplitPane>
-          </SplitPaneLeft>
-          <Divider className="separator-col" />
+	const [currQuote, setCurrQuote] = useState(1);
 
-          <SplitPaneRight />
-        </SplitPane>
-      </ReferencePaneCtxt.Provider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ReferencePaneCtxt.Provider value={{}}>
+				<SplitPane className="split-pane-row">
+					<SplitPaneLeft>
+						<SplitPane className="split-pane-col">
+							<SplitPaneTop />
+							<Divider className="separator-row" />
+							<SplitPaneBottom />
+						</SplitPane>
+					</SplitPaneLeft>
+					<Divider className="separator-col" />
+
+					<SplitPaneRight />
+				</SplitPane>
+			</ReferencePaneCtxt.Provider>
+		</div>
+	);
 }
 
 export default App;
--- src/components/SplitPane.js
+++ src/components/SplitPane.js
@@ -1,146 +1,132 @@
 import React, {
-    createRef,
-    useContext,
-    useEffect,
-    useRef,
-    useState,
-  } from "react";
-  import ReferencePaneCtxt from "./ReferencePaneCtxt";
-  import NoteTakingPaneCtxt from "./NoteTakingPaneCtxt";
-  
-  const SplitPane = ({ children, ...props }) => {
-    const [clientHeight, setClientHeight] = useState(null);
-    const [clientWidth, setClientWidth] = useState(null);
-    const yDividerPos = useRef(null);
-    const xDividerPos = useRef(null);
-  
-    const onMouseHoldDown = (e) => {
-      yDividerPos.current = e.clientY;
-      xDividerPos.current = e.clientX;
-    };
-  
-    const onMouseHoldUp = () => {
-      yDividerPos.current = null;
-      xDividerPos.current = null;
-    };
-  
-    const onMouseHoldMove = (e) => {
-      if (!yDividerPos.current && !xDividerPos.current) {
-        return;
-      }
-  
-      setClientHeight(clientHeight + e.clientY - yDividerPos.current);
-      setClientWidth(clientWidth + e.clientX - xDividerPos.current);
-  
-      yDividerPos.current = e.clientY;
-      xDividerPos.current = e.clientX;
-    };
-  
-    useEffect(() => {
-      document.addEventListener("mouseup", onMouseHoldUp);
-      document.addEventListener("mousemove", onMouseHoldMove);
-  
-      return () => {
-        document.removeEventListener("mouseup", onMouseHoldUp);
-        document.removeEventListener("mousemove", onMouseHoldMove);
-      };
-    });
-  
-    return (
-      <div {...props}>
-        <NoteTakingPaneCtxt.Provider
-          value={{
-            clientHeight,
-            setClientHeight,
-            clientWidth,
-            setClientWidth,
-            onMouseHoldDown,
-          }}
-        >
-          {children}
-        </NoteTakingPaneCtxt.Provider>
-      </div>
-    );
-  };
+	createRef,
+	useContext,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
+import ReferencePaneCtxt from "./ReferencePaneCtxt";
+import NoteTakingPaneCtxt from "./NoteTakingPaneCtxt";
 
-  export const Divider = (props) => {
-    const { onMouseHoldDown } = useContext(NoteTakingPaneCtxt);
-  
-    return <div {...props} onMouseDown={onMouseHoldDown} />;
-  };
-  
-  export const SplitPaneTop = (props) => {
-    const topRef = createRef();
-    const { clientHeight, setClientHeight } = useContext(NoteTakingPaneCtxt);
-    const { quotes, setCurrQuote } = useContext(ReferencePaneCtxt);
-  
-    useEffect(() => {
-      if (!clientHeight) {
-        setClientHeight(topRef.current.clientHeight);
-        return;
-      }
-  
-      topRef.current.style.minHeight = clientHeight + "px";
-      topRef.current.style.maxHeight = clientHeight + "px";
-    }, [clientHeight]);
-  
-    return (
-      <div {...props} className="split-pane-top" ref={topRef}>
-        <h1>Famous quotes:</h1>
-        <ul>
-          {quotes.map((el, i) => {
-            return (
-              <li key={i}>
-                <a href="#" onClick={() => setCurrQuote(el.id)}>
-                  {el.author}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  };
-  
-  export const SplitPaneBottom = (props) => {
-    const { currQuote } = useContext(ReferencePaneCtxt);
-  
-    return (
-      <div {...props} className="split-pane-bottom">
-        Current <b>quote id</b>: {currQuote}
-      </div>
-    );
-  };
-  
-  export const SplitPaneLeft = (props) => {
-    const topRef = createRef();
-    const { clientWidth, setClientWidth } = useContext(NoteTakingPaneCtxt);
-  
-    useEffect(() => {
-      if (!clientWidth) {
-        setClientWidth(topRef.current.clientWidth / 2);
-        return;
-      }
-  
-      topRef.current.style.minWidth = clientWidth + "px";
-      topRef.current.style.maxWidth = clientWidth + "px";
-    }, [clientWidth]);
-  
-    return <div {...props} className="split-pane-left" ref={topRef} />;
-  };
-  
-  export const SplitPaneRight = (props) => {
-    const { quotes, currQuote } = useContext(ReferencePaneCtxt);
-    const quote = quotes.find((el) => el.id === currQuote);
-  
-    return (
-      <div {...props} className="split-pane-right">
-        <div className="quote">
-          <blockquote>{quote.description}</blockquote>—{" "}
-          <span>{quote.author}</span>
-        </div>
-      </div>
-    );
-  };
-  
-  export default SplitPane; 
+const SplitPane = ({ children, ...props }) => {
+	const [clientHeight, setClientHeight] = useState(null);
+	const [clientWidth, setClientWidth] = useState(null);
+	const yDividerPos = useRef(null);
+	const xDividerPos = useRef(null);
+
+	const onMouseHoldDown = (e) => {
+		yDividerPos.current = e.clientY;
+		xDividerPos.current = e.clientX;
+	};
+
+	const onMouseHoldUp = () => {
+		yDividerPos.current = null;
+		xDividerPos.current = null;
+	};
+
+	const onMouseHoldMove = (e) => {
+		if (!yDividerPos.current && !xDividerPos.current) {
+			return;
+		}
+
+		setClientHeight(clientHeight + e.clientY - yDividerPos.current);
+		setClientWidth(clientWidth + e.clientX - xDividerPos.current);
+
+		yDividerPos.current = e.clientY;
+		xDividerPos.current = e.clientX;
+	};
+
+	useEffect(() => {
+		document.addEventListener("mouseup", onMouseHoldUp);
+		document.addEventListener("mousemove", onMouseHoldMove);
+
+		return () => {
+			document.removeEventListener("mouseup", onMouseHoldUp);
+			document.removeEventListener("mousemove", onMouseHoldMove);
+		};
+	});
+
+	return (
+		<div {...props}>
+			<NoteTakingPaneCtxt.Provider
+				value={{
+					clientHeight,
+					setClientHeight,
+					clientWidth,
+					setClientWidth,
+					onMouseHoldDown,
+				}}
+			>
+				{children}
+			</NoteTakingPaneCtxt.Provider>
+		</div>
+	);
+};
+
+export const Divider = (props) => {
+	const { onMouseHoldDown } = useContext(NoteTakingPaneCtxt);
+
+	return <div {...props} onMouseDown={onMouseHoldDown} />;
+};
+
+export const SplitPaneTop = (props) => {
+	const topRef = createRef();
+	const { clientHeight, setClientHeight } = useContext(NoteTakingPaneCtxt);
+	const { quotes, setCurrQuote } = useContext(ReferencePaneCtxt);
+
+	useEffect(() => {
+		if (!clientHeight) {
+			setClientHeight(topRef.current.clientHeight);
+			return;
+		}
+
+		topRef.current.style.minHeight = clientHeight + "px";
+		topRef.current.style.maxHeight = clientHeight + "px";
+	}, [clientHeight]);
+
+	return (
+		<div {...props} className="split-pane-top" ref={topRef}>
+			<h1>Famous quotes:</h1>
+			<ul>
+				<li>hi</li>
+			</ul>
+		</div>
+	);
+};
+
+export const SplitPaneBottom = (props) => {
+	const { currQuote } = useContext(ReferencePaneCtxt);
+
+	return (
+		<div {...props} className="split-pane-bottom">
+			Current <b>quote id</b>: {currQuote}
+		</div>
+	);
+};
+
+export const SplitPaneLeft = (props) => {
+	const topRef = createRef();
+	const { clientWidth, setClientWidth } = useContext(NoteTakingPaneCtxt);
+
+	useEffect(() => {
+		if (!clientWidth) {
+			setClientWidth(topRef.current.clientWidth / 2);
+			return;
+		}
+
+		topRef.current.style.minWidth = clientWidth + "px";
+		topRef.current.style.maxWidth = clientWidth + "px";
+	}, [clientWidth]);
+
+	return <div {...props} className="split-pane-left" ref={topRef} />;
+};
+
+export const SplitPaneRight = (props) => {
+	return (
+		<div {...props} className="split-pane-right">
+			<div className="quote">hteh yeyr</div>
+		</div>
+	);
+};
+
+export default SplitPane;
