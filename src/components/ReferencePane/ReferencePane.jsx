40afc8033533b8d5f65bcb5c95bbda4449012df4
S. G. Opferman <sophieopferman6@gmail.com>
2022-11-28 17:50:47 -0800

    can view PDF kind of

--- src/components/ReferencePane/ReferencePane.jsx
+++ src/components/ReferencePane/ReferencePane.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import { getAnnotationsDummy } from "../../data/dummy-annotations";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import bayes from "../../data/Bayes' Theorem.pdf";
 
 export default function ReferencePane(props) {
 	const [annotations, setAnnotations] = useState(getAnnotationsDummy());
 
+	const [numPages, setNumPages] = useState(null);
+
+	function onDocumentLoadSuccess({ numPages }) {
+		setNumPages(numPages);
+	}
+
 	return (
 		<div className="referencePane">
-			<ul>
+			{/* <ul>
 				{annotations.map((annotation, index) => {
 					return <li key={index}>{annotation.color}</li>;
 				})}
-			</ul>
+			</ul> */}
+
+			<Document file={bayes} onLoadSuccess={onDocumentLoadSuccess}>
+				{Array.from(new Array(numPages), (el, index) => (
+					<Page key={`page_${index + 1}`} pageNumber={index + 1} height={500} />
+				))}
+			</Document>
 		</div>
 	);
 }
